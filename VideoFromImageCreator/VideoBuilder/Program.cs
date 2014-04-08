703ec4f939f1d3ad0c13cabe5886ed449ce168bc
Berthold Roiser <berthold.roiser@gmail.com>
2014-04-08 13:21:31 +0200

    add test project

---
+++ VideoFromImageCreator/VideoBuilder/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VideoBuilder builder = new VideoBuilder();
+            builder = builder.AddPicture(new Picture( "C:\\Users\\Java\\Pictures\\Unbenannt2.PNG", 5000));
+            builder =  builder.AddPicture(new Picture("C:\\Users\\Java\\Pictures\\Unbenannt.PNG", 2000));
+            builder = builder.Height(800);
+            builder = builder.Width(800);
+            builder.Build("result.wmv");
+        }
+    }
+}
