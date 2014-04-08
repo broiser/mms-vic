7f3402c6db5909ee1c5d0122da520cbd55b9e004
Berthold Roiser <berthold.roiser@gmail.com>
2014-04-08 11:09:06 +0200

    add VideoBuilder

---
+++ VideoFromImageCreator/VideoFromImageCreator/Picture.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoFromImageCreator
+{
+    class Picture
+    {
+
+        public String Path { get; set; }
+        public int Duration { get; set; }
+
+    }
+}
