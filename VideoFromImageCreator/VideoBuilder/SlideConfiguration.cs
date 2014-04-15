e3116cf91d2a72c3b4b39a5438c7962f6cd70703
Berthold Roiser <berthold.roiser@gmail.com>
2014-04-15 13:59:53 +0200

    add Resource.cs, SlideConfiguration.cs

---
+++ VideoFromImageCreator/VideoBuilder/SlideConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoBuilder
+{
+    class SlideConfiguration
+    {
+        public SlideConfiguration(int duration)
+        {
+            this.Duration = duration;
+        }
+
+        public int Duration { get; set; }
+    }
+}
