068742f9658177f55298a151e45323957f42a021
ntsmwk <markus.weissenbek@gmx.at>
2014-04-17 20:20:20 +0200

    asdf

---
+++ VideoFromImageCreator/VideoFromImageCreator/Music.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoFromImageCreator
+{
+    class Music
+    {
+        public String Path { get; set; }
+
+        public Music(String path)
+        {
+            this.Path = path;
+        }
+
+        
+    }
+}
---
+++ VideoFromImageCreator/VideoFromImageCreator/Picture.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoFromImageCreator
+{
+    class Picture
+    {
+
+        public String Path { get; set; }
+        public int Duration { get; set; }
+
+        public Picture(String path, int duration)
+        {
+            this.Path = path;
+            this.Duration = duration;
+        }
+    }
+}
