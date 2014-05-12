220b3a813cc049291a9036d6bfdcf90784595ffe
PriinceGoerue <mehmet.goerue@hotmail.de>
2014-05-12 02:03:13 +0200

    Eventhandler implementet

--- VideoFromImageCreator/VideoFromImageCreator/MainWindow.xaml.cs
+++ VideoFromImageCreator/VideoFromImageCreator/MainWindow.xaml.cs
@@ -1,13 +1,17 @@
-﻿using System;
+﻿using BytescoutImageToVideoLib;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -58,32 +62,92 @@ namespace VideoFromImageCreator
 
         private void AddFolder_Click(object sender, RoutedEventArgs e)
         {
-            //TODO
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            DialogResult result = fbd.ShowDialog();
+
+            string[] files = Directory.GetFiles(fbd.SelectedPath);
+            for (int i = 0; i < files.Length; i++)
+            {
+                if (files[i].EndsWith(".jpg") || files[i].EndsWith(".jpeg") || files[i].EndsWith(".png") || files[i].EndsWith(".gif") || files[i].EndsWith(".bmp"))
+                {
+                    string path = files[i];
+                    int dur = DEFAULT_DURATION;
+                    //TODO extract correct Effect
+                    TransitionEffectType inEffect = TransitionEffectType.teNone;
+                    TransitionEffectType outEffect = TransitionEffectType.teNone;
+                    VisualEffectType visualEffectType = VisualEffectType.veNone;
+
+                    Picture p = new Picture(path, dur, inEffect, outEffect, visualEffectType);
+                    AddPicture(p);
+                    
+                }
+            }
+            
         }
 
         private void AddTransitionEffect_Click(object sender, RoutedEventArgs e)
         {
-            //TODO
+            Picture p = (Picture)pictureGrid.SelectedItem;
+            int position = pictureGrid.Items.IndexOf(pictureGrid.SelectedCells[0].Item);
+            
+            pictures.Remove(p);
+            //p.InTransitionEffect= AUSWAHL
+            pictures.Insert(position, p);
         }
 
         private void AddVisualEffect_Click(object sender, RoutedEventArgs e)
         {
-            //TODO
+            Picture p = (Picture)pictureGrid.SelectedItem;
+            int position = pictureGrid.Items.IndexOf(pictureGrid.SelectedCells[0].Item);
+
+            pictures.Remove(p);
+            //p.visualEffectType= AUSWAHL
+            pictures.Insert(position, p);
         }
 
         private void MoveUp_Click(object sender, RoutedEventArgs e)
         {
-            //TODO
+            Picture p = (Picture)pictureGrid.SelectedItem;
+            int position = pictureGrid.Items.IndexOf(pictureGrid.SelectedCells[0].Item);
+            if(position-1 >=0)
+            {
+                pictures.Remove(p);
+                pictures.Insert(position - 1, p);
+                this.pictureGrid.Items.Refresh();
+            }
+
         }
 
         private void MoveDown_Click(object sender, RoutedEventArgs e)
         {
-            //TODO
+            Picture p = (Picture)pictureGrid.SelectedItem;
+            int position = pictureGrid.Items.IndexOf(pictureGrid.SelectedCells[0].Item);
+            if(position+1 < pictures.Count)
+            {
+                pictures.Remove(p);
+                pictures.Insert(position + 1, p);
+                this.pictureGrid.Items.Refresh();
+            }
+
         }
 
         private void SetDuration_Click(object sender, RoutedEventArgs e)
         {
-            //TODO
+            Picture p = (Picture)pictureGrid.SelectedItem;
+
+            if(p == null)
+            {
+                List<Picture> helpPictures = new List<Picture>();
+                
+                foreach (Picture pics in pictures)
+                {
+                    pics.Duration=10;
+                    helpPictures.Add(pics);
+                }
+                pictures = helpPictures;
+                this.pictureGrid.Items.Refresh();
+            }
+            
         }
 
         private void AddMusic_Click(object sender, RoutedEventArgs e)
@@ -110,6 +174,7 @@ namespace VideoFromImageCreator
             builder = builder.Height(800).Width(800);
             builder.Build(RESULT);
         }
+
 
     }
 }
